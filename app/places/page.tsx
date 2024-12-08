183d74ee04c768fc8701e18290e3085d8977ae98
Harry Thomas <hsthomas@verisk.com>
2024-12-07 17:18:26 -0700

    places grid improvements

--- app/places/page.tsx
+++ app/places/page.tsx
@@ -16,14 +16,53 @@ const places: Place[] = [
     imageUrl:
       'https://locable-assets-production.s3.amazonaws.com/uploads/resource/file/656616/suncrest_203.jpeg?timestamp=1733067996',
   },
+  {
+    id: '3',
+    name: 'Adams All Abilities Park',
+    description: `This is William's favorite park to come to in Utah county! He could hang out here for HOURS!`,
+    imageUrl:
+      'https://i.ytimg.com/vi/pAcpEbxCCPk/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDOl1u-VepMBwNvzFfimUUHJ5zarA',
+  },
+  {
+    id: '4',
+    name: 'Suncrest Park',
+    description: `Not Will's favorite park, but it gets the job done when we are in a pinch and don't want to drive any further.`,
+    imageUrl:
+      'https://locable-assets-production.s3.amazonaws.com/uploads/resource/file/656616/suncrest_203.jpeg?timestamp=1733067996',
+  },
+  {
+    id: '5',
+    name: 'Adams All Abilities Park',
+    description: `This is William's favorite park to come to in Utah county! He could hang out here for HOURS!`,
+    imageUrl:
+      'https://i.ytimg.com/vi/pAcpEbxCCPk/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDOl1u-VepMBwNvzFfimUUHJ5zarA',
+  },
+  {
+    id: '6',
+    name: 'Suncrest Park',
+    description: `Not Will's favorite park, but it gets the job done when we are in a pinch and don't want to drive any further.`,
+    imageUrl:
+      'https://locable-assets-production.s3.amazonaws.com/uploads/resource/file/656616/suncrest_203.jpeg?timestamp=1733067996',
+  },
 ]
 
 export default function Places() {
-  return places.map((place) => {
-    return (
-      <div key={place.id}>
-        <PlaceComponent place={place}></PlaceComponent>
-      </div>
-    )
-  })
+  return (
+    <div
+      className={
+        'flex flex-wrap justify-start gap-8 p-8' // Flex container with wrapping and spacing
+      }
+    >
+      {places.map((place) => {
+        return (
+          <div
+            key={place.id}
+            className={`// Two columns on tablets // Three columns on desktops // One column on mobile // Optional: Limit max size per card max-w-[400px] flex-grow basis-full sm:basis-1/2 lg:basis-1/3`}
+          >
+            <PlaceComponent place={place}></PlaceComponent>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
--- components/custom/place-component.tsx
+++ components/custom/place-component.tsx
@@ -16,33 +16,24 @@ import { AudioLines } from 'lucide-react'
 export const PlaceComponent = ({ place }: { place: Place }) => {
   const key = `place-${place.id}`
   return (
-    <div className={cn('p-8')}>
-      <Card
-        className={cn('cursor-pointer')}
-        onClick={() => speakText(place.name)}
-      >
-        <CardHeader>
-          <CardTitle>
-            <div className="flex items-center gap-4">
-              {place.name}
-              <AudioLines />
-            </div>
-
-            {/* <Button className={cn('mx-2')} size="icon">
-              <AudioLines />
-            </Button> */}
-          </CardTitle>
-          <CardDescription>{place.description}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Image
-            height={300}
-            width={300}
-            src={place.imageUrl}
-            alt={key}
-          ></Image>
-        </CardContent>
-      </Card>
-    </div>
+    <Card
+      className={cn('min-h-full cursor-pointer')}
+      onClick={() => speakText(place.name)}
+    >
+      <CardHeader>
+        <CardTitle>
+          <div className="flex items-center gap-4">
+            {place.name}
+            <AudioLines />
+          </div>
+        </CardTitle>
+        <CardDescription className={cn('visible:false')}>
+          {place.description}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Image height={300} width={300} src={place.imageUrl} alt={key}></Image>
+      </CardContent>
+    </Card>
   )
 }
