34cc5090e25b4dbbacdc35386ee8ba16fdf5fec5
Harry Thomas <hsthomas@verisk.com>
2024-12-24 13:47:12 -0700

    responsive design tweaks

--- components/custom/hero.tsx
+++ components/custom/hero.tsx
@@ -1,6 +1,7 @@
 import { GitHubLogoIcon } from '@radix-ui/react-icons'
 import { Button } from '../ui/button'
 import Link from 'next/link'
+import { CirclePlayIcon } from 'lucide-react'
 
 export const Hero = () => {
   return (
@@ -14,7 +15,7 @@ export const Hero = () => {
           </h1>
         </main>
 
-        <p className="mx-auto text-xl text-muted-foreground md:w-6/12">
+        <p className="mx-auto w-11/12 text-xs text-muted-foreground md:w-8/12 md:text-xl">
           VoiceBridge is a
           <span className="border-gradient inline border-b-2 bg-gradient-to-r from-[#aa00ff] to-[#00cc99] bg-clip-text text-transparent">
             &nbsp;free and open source&nbsp;
@@ -24,17 +25,25 @@ export const Hero = () => {
           food, and accessing calming music playlists.
         </p>
 
-        <div className="space-y-4 md:space-x-4">
-          <Link href="/login">
-            <Button className="w-full md:w-1/5">Get Started</Button>
-          </Link>
+        <div className="flex w-full flex-col items-center justify-center gap-4 md:flex-row">
+          <div className="center grid max-w-screen-sm grid-cols-1 items-center justify-center gap-4 self-center md:grid-cols-2">
+            <Link href="/login">
+              <Button className="w-full md:max-w-none">
+                <CirclePlayIcon className="ml-2 h-5 w-5" />
+                Get Started
+              </Button>
+            </Link>
 
-          <Link target="_blank" href="https://github.com/harryt04/VoiceBridge">
-            <Button variant="outline" className="w-full md:w-1/5">
-              Github Repository
-              <GitHubLogoIcon className="ml-2 h-5 w-5" />
-            </Button>
-          </Link>
+            <Link
+              target="_blank"
+              href="https://github.com/harryt04/VoiceBridge"
+            >
+              <Button variant="outline" className="w-full md:max-w-none">
+                <GitHubLogoIcon className="ml-2 h-5 w-5" />
+                Github Repository
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
