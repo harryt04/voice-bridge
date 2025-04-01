e566308823aa8f60dceccdb2d7dc56ed85d5449f
Harry Thomas <hsthomas@verisk.com>
2025-03-31 19:05:56 -0600

    revert foods page stuff

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -1,43 +1,13 @@
+import GenericItemsPage, {
+  GenericPageInfo,
+} from '@/components/custom/generic-items-page'
 import { NoResultsComponent } from '@/components/custom/no-results-component'
 import { AppleIcon } from 'lucide-react'
-import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs'
-import { getMongoClient, mongoDBConfig } from '@/lib/mongo-client'
-import { currentUser } from '@clerk/nextjs/server'
-import ItemsList from '@/components/custom/items-list'
-import { Food } from '@/models/food'
+import React from 'react'
 
-export default async function Foods() {
-  const user = await currentUser()
-  let initialFoods: Food[] = []
-  let speakerId = ''
-
-  if (user) {
-    const client = await getMongoClient()
-    const db = client.db(mongoDBConfig.dbName)
-
-    // First get the selected speaker for this user
-    const speakersCollection = db.collection(mongoDBConfig.collections.speakers)
-    const speaker = await speakersCollection.findOne({
-      userId: user.id,
-      isSelected: true,
-    })
-
-    if (speaker) {
-      speakerId = speaker._id.toString()
-      // Then get the foods for this speaker
-      const foodsCollection = db.collection(mongoDBConfig.collections.foods)
-      const documents = await foodsCollection
-        .find({ speakerId: speakerId })
-        .toArray()
-
-      initialFoods = documents.map((doc) => ({
-        ...doc,
-        _id: doc._id.toString(),
-      })) as Food[]
-    }
-  }
-
-  const pageInfo = {
+function Foods() {
+  const pageInfo: GenericPageInfo = {
+    listModelName: 'foods',
     editModelName: 'food',
     singularLabel: 'food',
     pluralLabel: 'foods',
@@ -49,19 +19,11 @@ export default async function Foods() {
       />
     ),
   }
-
   return (
     <>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-      <SignedIn>
-        <ItemsList
-          initialItems={initialFoods}
-          pageInfo={pageInfo}
-          speakerId={speakerId}
-        />
-      </SignedIn>
+      <GenericItemsPage pageInfo={pageInfo} />
     </>
   )
 }
+
+export default Foods
