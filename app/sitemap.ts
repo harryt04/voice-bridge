375794356ede3610f3ebf807448de6edcbd53f99
Harry Thomas <harrison.thomas04@gmail.com>
2026-07-17 21:42:52 -0600

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+  const baseUrl = `https://vb.harryt.dev`
 
   return [
     {
